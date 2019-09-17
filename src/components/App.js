08160d5eb1a3fac93b9b2236627c9be972c2afd4
Patrick <pat.mcagnoni@gmail.com>
2019-09-17 10:11:06 -0300

    fx refactor

--- src/components/App.js
+++ src/components/App.js
@@ -5,19 +5,37 @@ import Sidebar from './Sidebar/Sidebar';
 import Content from './Content/Content';
 
 import { SET_USER } from '../store/reducers/mainReducer';
-
+import { LOAD_TRANSACTIONS, FILTER_TRANSACTIONS } from '../store/reducers/transactionsReducer';
 
 function App(props) {
 
-  const {setUserData} = props;
+  const {
+    setUserData, 
+    transactions, 
+    loadTransactions, 
+    filterTransactions, 
+    date
+  } = props;
 
   useEffect(() => {
       // setUserData({name:{first:'john', last:'doe'}})
       fetch('https://randomuser.me/api/')
-        .then(res => res.json())
+        .then(res => res.json()) 
         .then(response => setUserData(response.results[0]))
         .catch(e => console.log('error fetching data', e) )
+      
+      const transactions = JSON.parse(localStorage.getItem('transactions'));
+      if(transactions){
+  
+        loadTransactions(transactions);
+      }
   },[])
+
+  useEffect(()=> {
+        
+    localStorage.setItem('transactions', JSON.stringify(transactions));
+    filterTransactions(date);
+    }, [transactions])
 
   return (
     <div className="app-grid">
@@ -30,8 +48,31 @@ function App(props) {
   );
 }
 
+const mapStateToProps = state => {
+  return {
+      transactions: state.transactions.transactions,
+      date: state.dateSlider.date
+  }
+}
+
 const mapDispatchToProps = dispatch => {
   return {
+
+    loadTransactions: transactions => {
+      const action ={
+        type: LOAD_TRANSACTIONS,
+        transactions
+      } 
+      dispatch(action)
+    },
+    
+    filterTransactions: date => {
+      const action = {
+          type: FILTER_TRANSACTIONS,
+          date
+      }
+      dispatch(action);
+    },
     setUserData: user => {
       const action ={
         type: SET_USER,
@@ -42,4 +83,4 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);

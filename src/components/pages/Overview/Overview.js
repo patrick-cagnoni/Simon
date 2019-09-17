0e553e5eec992f57e8bbbe47f2d15f798a681a97
Patrick <pat.mcagnoni@gmail.com>
2019-09-17 09:20:13 -0300

    pagina overview adicionada

--- src/components/pages/Overview/Overview.js
+++ src/components/pages/Overview/Overview.js
@@ -1,10 +1,28 @@
 import React from 'react';
 
-const Overview = () => {
-    return ( 
-        <div className="text-center mt-5">
-            <h1>Under Construction</h1>
+import DateSlider from '../../DateSlider/DateSlider';
+import OverviewTab from './OverviewTab';
 
+const Overview = () => {
+
+    const total = 230;
+
+    return ( 
+        <div className="overview">
+            <DateSlider />
+            <div className="overview-content card">
+                <div className="overview-tabs">
+                    <div className="overview-tab tab-expense">
+                        <div className="overview-tab-title text-danger selected mr-5">Expense | $ {total}</div>
+                    </div>
+                    <div className="overview-tab tab-income">
+                        <div className="overview-tab-title text-success">Income | $ {total}</div>
+                    </div>
+                </div>
+                <div className="overview-tab-content">
+                    <OverviewTab />
+                </div>
+            </div>
         </div>
         
      );

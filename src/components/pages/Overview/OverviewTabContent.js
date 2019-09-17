b397d72be3df158b0d3bb02186188dbe2cee3313
Patrick <pat.mcagnoni@gmail.com>
2019-09-17 14:08:16 -0300

    arquivo renomeado

---
+++ src/components/pages/Overview/OverviewTabContent.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import Chart from 'react-apexcharts';
+
+
+const OverviewTabContent = props => {
+
+    const { transactions } = props;
+
+   const state = {
+        options: {
+            labels:['cat-1','cat-2','cat-3',],
+            legend: {show:false}
+        },
+        series: [
+            11, 23, 56
+        ],
+  }
+    const tableData = [
+        {
+            percentage: 13,
+            category: 'food',
+            value: 100
+        }
+    ]
+    
+    return (
+        <div className="tab-content">
+            <div className="tab-graph">
+                <Chart series={state.series} options={state.options} type="donut" width="350"/>
+            </div>
+            <div className="tab-table-wrapper">
+                <table className="table tab-table">
+                    <tbody>
+                        {
+                            tableData.map(td => (
+                                <tr>
+                                    <td className="tab-tabledata-percentage">{td.percentage} %</td>
+                                    <td className="tab-tabledata-category">{td.category}</td>
+                                    <td className="tab-tabledata-value">$ {td.value}</td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+}
+ 
+export default OverviewTabContent;

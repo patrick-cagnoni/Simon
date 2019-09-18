f3d8773e8691f641e86005cd0b08c6fb3d3ad459
Patrick <pat.mcagnoni@gmail.com>
2019-09-18 14:01:12 -0300

    grafico e tabela adicionados

--- src/components/pages/Overview/Overview.js
+++ src/components/pages/Overview/Overview.js
@@ -1,11 +1,72 @@
-import React from 'react';
-
+import React,{useState} from 'react';
+import { connect } from 'react-redux';
 import DateSlider from '../../DateSlider/DateSlider';
-import OverviewTab from './OverviewTab';
+import OverviewTabContent from './OverviewTabContent';
 
-const Overview = () => {
 
-    const total = 230;
+const Overview = props => {
+
+    const [tab, setTab] = useState('Expense');
+
+    const { transactions } = props;
+
+        const totalByType = calculateTotalByType(transactions);
+        const totalByCategory = calculateTotalByCategory(transactions.filter(t => t.type === tab));
+        const chartData = {
+            options:{
+                labels: Object.keys(totalByCategory),
+                legend: {show:false}
+            },
+            series: Object.values(totalByCategory)
+        }
+        const tableData = (totalByCategory) => {
+            let data =[];
+            for(let key in totalByCategory){
+                const amount = totalByCategory[key];
+                const total = Object.values(totalByCategory).reduce((acc, cur)=> acc + cur);
+                const percentage =  amount * 100 / total;
+                const category = key;
+                data.push(
+                    {
+                        percentage,
+                        category,
+                        amount  
+                    }
+                )
+            }
+            return data;
+        }
+
+
+    function calculateTotalByCategory(transactions){
+        let obj = {};
+
+        transactions.forEach(t => {
+            if(obj[t.category]){
+                obj[t.category] = obj[t.category] + parseFloat(t.amount);
+            }
+            else{
+                obj[t.category] = parseFloat(t.amount);
+            }
+        })
+        return obj;
+    }
+
+    function calculateTotalByType(transactions){
+        let expense = 0;
+        let income = 0;
+
+        transactions.forEach(t => {
+            if(t.type === 'Expense'){
+                expense = expense + parseFloat(t.amount);
+            }
+            else {
+                income = income + parseFloat(t.amount);
+            }
+        });
+
+        return { expense, income };
+    }
 
     return ( 
         <div className="overview">
@@ -13,19 +74,34 @@ const Overview = () => {
             <div className="overview-content card">
                 <div className="overview-tabs">
                     <div className="overview-tab tab-expense">
-                        <div className="overview-tab-title text-danger selected mr-5">Expense | $ {total}</div>
+                        <div 
+                            className={`overview-tab-title text-danger mr-5 ${tab === 'Expense'? 'selected':null}`}
+                            onClick={() => setTab('Expense')}
+                        >Expense | $ {totalByType.expense.toFixed(2)}</div>
                     </div>
                     <div className="overview-tab tab-income">
-                        <div className="overview-tab-title text-success">Income | $ {total}</div>
+                        <div 
+                            className={`overview-tab-title text-success ${tab === 'Income'? 'selected': null}`}
+                            onClick={() => setTab('Income')}
+                        >Income | $ {totalByType.income.toFixed(2)}</div>
                     </div>
                 </div>
-                <div className="overview-tab-content">
-                    <OverviewTab />
-                </div>
+                {transactions.length > 0?
+                    <div className="overview-tab-content">
+                        <OverviewTabContent chartData={chartData} tableData={tableData(totalByCategory)}/>
+                    </div>
+                :
+                <div className="no-data"><h1 className="text-center">No data available</h1></div>
+            }
             </div>
         </div>
         
      );
 }
  
-export default Overview;
+const mapStateToProps = state =>{
+    return {
+        transactions: state.transactions.filteredTransactions
+    }
+}
+export default connect(mapStateToProps)(Overview);
--- src/components/pages/Overview/OverviewTabContent.js
+++ src/components/pages/Overview/OverviewTabContent.js
@@ -5,39 +5,22 @@ import Chart from 'react-apexcharts';
 
 const OverviewTabContent = props => {
 
-    const { transactions } = props;
+    const { chartData, tableData } = props;
 
-   const state = {
-        options: {
-            labels:['cat-1','cat-2','cat-3',],
-            legend: {show:false}
-        },
-        series: [
-            11, 23, 56
-        ],
-  }
-    const tableData = [
-        {
-            percentage: 13,
-            category: 'food',
-            value: 100
-        }
-    ]
-    
     return (
         <div className="tab-content">
             <div className="tab-graph">
-                <Chart series={state.series} options={state.options} type="donut" width="350"/>
+                <Chart series={chartData.series} options={chartData.options} type="donut" width="350"/>
             </div>
             <div className="tab-table-wrapper">
                 <table className="table tab-table">
                     <tbody>
                         {
                             tableData.map(td => (
-                                <tr>
-                                    <td className="tab-tabledata-percentage">{td.percentage} %</td>
+                                <tr key={td.category}>
+                                    <td className="tab-tabledata-percentage">{td.percentage.toFixed(1)} %</td>
                                     <td className="tab-tabledata-category">{td.category}</td>
-                                    <td className="tab-tabledata-value">$ {td.value}</td>
+                                    <td className="tab-tabledata-value">$ {td.amount.toFixed(2)}</td>
                                 </tr>
                             ))
                         }

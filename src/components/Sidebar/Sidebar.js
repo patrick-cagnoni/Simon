8330f72e04bf3d9871d4cb0cfe35ed827e4d86a2
Patrick <pat.mcagnoni@gmail.com>
2019-09-20 10:11:06 -0300

    adicionado className

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -17,26 +17,32 @@ const Sidebar = props => {
     return ( 
         <div className="sidebar">
             <div className="sidebar-brand">
-                <Link to="/" className="site-name text-light"><span style={{fontSize: '1.6rem'}}>SIMON</span> <br/> <span>Simple Money Manager</span></Link>
+                <Link 
+                    to="/" 
+                    className="site-name text-light">
+                        <span style={{fontSize: '1.6rem'}}>SIMON</span> 
+                        <br/> 
+                        <span className="sidebar-brand-desc">Simple Money Manager</span>
+                </Link>
             </div>
             <ul className="sidebar-list">
                 <li className="sidebar-item">
                     
                     <Link to='/overview' className={`sidebar-link  ${getPage() === 'overview'? 'selected': null }`}>
-                        <span className="sidebar-icon mr-3"><Icon icon={faChartPie}/></span>
-                        Overview
+                        <span className="sidebar-icon"><Icon icon={faChartPie}/></span>
+                        <span className="sidebar-link-text">Overview</span>
                     </Link>
                 </li>
                 <li className="sidebar-item">
                     <Link to='/transactions' className={`sidebar-link  ${getPage() === 'transactions'? 'selected': null }`}>
-                        <span className="sidebar-icon mr-3"><Icon icon={faExchangeAlt}/></span>
-                        Transactions
+                        <span className="sidebar-icon"><Icon icon={faExchangeAlt}/></span>
+                        <span className="sidebar-link-text">Transactions</span>
                     </Link>
                 </li>
                 <li className="sidebar-item">
                     <Link to='/categories' className={`sidebar-link  ${getPage() === 'categories'? 'selected': null }`}>
-                    <span className="sidebar-icon mr-3"><Icon icon={faBoxes}/></span>
-                        Categories
+                    <span className="sidebar-icon"><Icon icon={faBoxes}/></span>
+                        <span className="sidebar-link-text">Categories</span>
                     </Link>
                     
                 </li>

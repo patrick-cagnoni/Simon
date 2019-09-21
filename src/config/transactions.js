89dc021600994bf336fdb20981f97ee418382e30
Patrick <pat.mcagnoni@gmail.com>
2019-09-20 22:44:00 -0300

    config contendo objetos importantes para o funcionamento do app

---
+++ src/config/transactions.js
@@ -0,0 +1,28 @@
+export const categoriesOpt = {
+
+    income: [
+        "Paycheck",
+        "Allowance",
+        "Bonus",
+        "Other"
+    ],
+    expense: [
+        "Food",
+        "Social Life", 
+        "Transportation", 
+        "Beauty",
+        "Health",
+        "Education",
+        "Gift",
+        "Household",
+        "Insurance",
+        "Tax", 
+        "Other"
+    ]
+
+}
+
+export const typeOpt = [
+    'Expense',
+    'Income'
+]

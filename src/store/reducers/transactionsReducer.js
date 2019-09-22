d08219372c52b6892d657720109ee7504961b950
Patrick <pat.mcagnoni@gmail.com>
2019-09-22 19:22:05 -0300

    alteracao na acao de filtrar transacoes

--- src/store/reducers/transactionsReducer.js
+++ src/store/reducers/transactionsReducer.js
@@ -1,5 +1,5 @@
 
-import { isSameMonth, parseISO } from 'date-fns'
+import { isSameMonth, parseISO, isSameDay } from 'date-fns'
 
 export const SELECT_TRANSACTION = "SELECT_TRANSACTION";
 export const ADD_TRANSACTION = "ADD_TRANSACTION";
@@ -10,7 +10,8 @@ export const FILTER_TRANSACTIONS = "FILTER_TRANSACTIONS";
 
 const initialState ={
     transactions: [],
-    filteredTransactions: []
+    filteredTransactions: [],
+    transactionsByDay: []
 }
 
 export default function transactionsReducer (state = initialState, action){
@@ -46,7 +47,36 @@ export default function transactionsReducer (state = initialState, action){
                 return isSameMonth(date1, date2);
             });
 
-            return {...state, filteredTransactions};
+            let transactionsByDay= [];
+            filteredTransactions.map(t => {
+                let sameDayTransaction =  transactionsByDay.find(dt => isSameDay(new Date(dt.date), new Date(t.date)));
+                if(transactionsByDay.length === 0 || !sameDayTransaction ){
+                    let dayTransaction = {list:[]};
+                    dayTransaction.date = t.date
+                    dayTransaction.totalIncome = t.type === 'Income'? t.amount: 0;
+                    dayTransaction.totalExpense = t.type === 'Expense'? t.amount: 0;
+                    dayTransaction.list.push(t);
+                    transactionsByDay.push(dayTransaction);
+                }
+                else {
+                    let index = transactionsByDay.indexOf(sameDayTransaction);
+                    transactionsByDay[index].list.push(t);
+                    if(t.type === 'Income'){
+
+                        transactionsByDay[index].totalIncome = parseFloat(transactionsByDay[index].totalIncome) + parseFloat(t.amount);
+                    }
+                    else {
+                        transactionsByDay[index].totalExpense = parseFloat(transactionsByDay[index].totalExpense) + parseFloat(t.amount);
+
+                    }
+                }
+            })
+            transactionsByDay.sort((a,b) => {
+                if(a.date > b.date) return -1;
+                else return 1;
+            })
+
+            return {...state, transactionsByDay};
         }
 
         case SELECT_TRANSACTION:
